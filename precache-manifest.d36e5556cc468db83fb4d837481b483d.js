0e899540ba1875724f853fbf1e4d150b24ab9abb
khonoridez <oznyakis@gmail.com>
2019-12-06 13:39:58 -0500

    Updates

---
+++ precache-manifest.d36e5556cc468db83fb4d837481b483d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057d038b8fa3657eb58f885e5c3a269c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d2528d08fda52eb60bc3",
+    "url": "/monsters-rolodex/static/css/main.214de543.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2528d08fda52eb60bc3",
+    "url": "/monsters-rolodex/static/js/main.b8c133c3.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
